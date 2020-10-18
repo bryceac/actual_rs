d086fb92272adadeac629b0622d593b75feb4f3e
Bryce Campbell <tonyhawk2100@gmail.com>
2020-10-17 17:17:24 -0700

    implement calculation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use serde_json;
+use std::{ fs, io, path::Path};
+
 fn main() {
-    println!("Hello, world!");
+    let unit = "GB";
+
+    let calculation = calculate(250, unit);
+
+    println!("Approximate capacity is: {} {}", calculation, unit)
+}
+
+fn load_file(file: &Path) -> Result<String, io::Error> {
+    fs::read_to_string(file)
+}
+
+fn units() -> Vec<String> {
+    let file_path = Path::new("units.json");
+    
+    let content = match load_file(&file_path) {
+        Ok(contents) => Some(contents),
+        _ => None
+    }.expect("Could not load file");
+
+    let units: Vec<String> = serde_json::from_str(&content).expect("Could not decode file");
+
+    return units;
+}
+
+fn calculate(size: u32, unit: &str) -> f64 {
+    let index = units().iter().position(|u| u == unit).expect("unit not found");
+
+    (f64::from(size)*1000_f64.powf(f64::from((index+1) as u32)))/1024_f64.powf(f64::from((index +1) as u32))
 }
