e8d89596164f61dfdb4734e67486a58226930e61
Bryce Campbell <tonyhawk2100@gmail.com>
2020-10-17 17:58:11 -0700

    fix parsing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
+use clap::{ Arg, App};
 use serde_json;
 use std::{ fs, io, path::Path};
 
 fn main() {
-    let unit = "GB";
 
-    let calculation = calculate(250, unit);
+    let matches = App::new("Actual Storage")
+        .version("0.1")
+        .author("Bryce Campbell <tonyhawk2100@gmail.com>")
+        .about("Calculates the usable storage of a secondary storage devive with a specified capacity.")
+        .arg(Arg::new("size")
+        .about("the nurmerical capacity")
+        .index(1)
+        .required(true))
+        .arg(Arg::new("unit")
+        .about("unit of measure for storage capacity")
+        .required(true)
+        .takes_value(true)
+        .index(2))
+    .get_matches();
+
+    let size: u32 = matches.value_of("size").unwrap().parse().unwrap();
+    let unit: &str = matches.value_of("unit").unwrap();
+
+    let calculation = calculate(size, unit);
 
     println!("Approximate capacity is: {:.2} {}", calculation, unit)
 }
